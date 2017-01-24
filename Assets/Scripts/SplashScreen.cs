e0fe8e8c40fc13ee7f833d74ece0b5c368fd7f23
Jack Ross, III <mrjackross@gmail.com>
2017-01-23 20:30:05 -0500

    Changed color of victory text. Also started work on a splash/title screen for the game

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+    /**
+     * Text saying to press any key to begin
+     */
+    public Text pressAnyKeyText;
+
+    /**
+     * Whether or not pressing a key will have any affect
+     */
+    protected bool ready = false;
+
+    /**
+     * Clear out the text and display after 5 seconds
+     */
+    void Start()
+    {
+        pressAnyKeyText.text = "";
+        IEnumerator coroutine = this.waitSomeTime(5);
+        StartCoroutine(coroutine);
+    }
+
+    /**
+     * Check for any keys being pressed
+     */
+    void Update() {
+        if (Input.anyKeyDown & ready) {
+            SceneManager.LoadScene("MiniGame", LoadSceneMode.Single);
+        }
+    }
+
+    protected IEnumerator waitSomeTime(int seconds) {
+        yield return new WaitForSeconds(seconds);
+        pressAnyKeyText.text = "Press Any Key to Begin!";
+        this.ready = true;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# unity-ball-tutorial
+# Unity Ball Game
+
+This game started off with me just following one of Unity's guides for creating a simple game, but after finishing the guide, I kept adding more features to keep learning more about Unity and how it works.
